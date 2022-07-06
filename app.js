c8913c7a45123580e5edf71889093f7a71789143
Satish Thapa <you@example.com>
2022-07-06 18:44:27 +0545

    V3 Heroku and Mongoose

--- app.js
+++ app.js
@@ -148,6 +148,10 @@ app.get("/about", function(req, res){
 //   res.render("list", {listTitle: "Work List", newListItems: workItems});
 // });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+// app.listen(3000, function() {
+//   console.log("Server started on port 3000");
+// });
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server Up and Running");
 });
+
